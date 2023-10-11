f1078cbdb76e0c83035c8fc1f4ad588800221af6
willyycn <willyy.cn@gmail.com>
2023-10-11 15:49:35 -0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -24,34 +24,6 @@ export LANG="en_US";
 export LANGUAGE="en_US";
 export LC_ALL="en_US";
 
-function InstallJQ() {
-    #安装JQ
-    if [ -e "/etc/redhat-release" ];then
-        echo -e "${Font_Green}正在安装依赖: epel-release${Font_Suffix}";
-        yum install epel-release -y -q > /dev/null;
-        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
-        yum install jq -y -q > /dev/null;
-        elif [[ $(cat /etc/os-release | grep '^ID=') =~ ubuntu ]] || [[ $(cat /etc/os-release | grep '^ID=') =~ debian ]];then
-        echo -e "${Font_Green}正在更新软件包列表...${Font_Suffix}";
-        apt-get update -y > /dev/null;
-        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
-        apt-get install jq -y > /dev/null;
-        elif [[ $(cat /etc/issue | grep '^ID=') =~ alpine ]];then
-        apk update > /dev/null;
-        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
-        apk add jq > /dev/null;
-    else
-        echo -e "${Font_Red}请手动安装jq${Font_Suffix}";
-        exit;
-    fi
-}
-
-function PharseJSON() {
-    # 使用方法: PharseJSON "要解析的原JSON文本" "要解析的键值"
-    # Example: PharseJSON ""Value":"123456"" "Value" [返回结果: 123456]
-    echo -n $1 | jq -r .$2;
-}
-
 function PasteBin_Upload() {
     local uploadresult="$(curl -fsL -X POST \
         --url https://paste.ubuntu.com \
@@ -113,17 +85,6 @@ function MediaUnlockTest_Netflix() {
 function MediaUnlockTest() {
     MediaUnlockTest_Netflix ${1};
 }
-
-curl -V > /dev/null 2>&1;
-if [ $? -ne 0 ];then
-    echo -e "${Font_Red}Please install curl${Font_Suffix}";
-    exit;
-fi
-
-jq -V > /dev/null 2>&1;
-if [ $? -ne 0 ];then
-    InstallJQ;
-fi
 
 echo " ** 正在测试IPv4解锁情况" && echo " ** 正在测试IPv4解锁情况" >> ${LOG_FILE};
 check4=`ping 1.1.1.1 -c 1 2>&1`;
